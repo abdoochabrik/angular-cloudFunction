2ea9d254194d0f12b9a3f5a2b01de52dbd0d87ee
Dell <abdoo.chbrik@gmail.com>
2021-06-11 14:26:46 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-//Install express server
+
+/*//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -14,3 +15,19 @@ res.sendFile(path.join(__dirname+'/dist/inpt-cloud-mamgas/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 2121);
+*/
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+ app.use(express.static('./dist/TPSERVERLESS'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/TPSERVERLESS/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
