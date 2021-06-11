501d5cf539c9f4aa30c09ed16257f51a1da57de3
Dell <abdoo.chbrik@gmail.com>
2021-06-11 17:02:32 +0200

    another one

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-
-/*//Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -10,24 +9,9 @@ app.use(express.static(__dirname + '/dist/inpt-cloud-mamgas'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/inpt-cloud-mamgas/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 2121);
-*/
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
- app.use(express.static('./dist/TPSERVERLESS'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/TPSERVERLESS/'}),
-);
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
